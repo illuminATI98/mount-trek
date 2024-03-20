295ec2c5df16d52e8b44f386e292d72f1b9ad1a9
Attila Dobi <attiladobi100@gmail.com>
2024-03-21 00:14:56 +0100

    Import Navbar, Home and styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.scss'
+import { Navbar } from './components';
+import { Home } from './container';
 
 function App() {
   
   return (
-    <>
-    </>
+    <div className='app'>
+      <Navbar />
+
+      <section className='app__container'>
+        <Home />
+      </section>
+
+    </div>
   )
 }
 

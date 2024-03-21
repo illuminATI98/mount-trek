03ac875e81ae94c8b5aa91d1c7f006ddb5dc6b7f
Attila Dobi <attiladobi100@gmail.com>
2024-03-21 19:37:20 +0100

    Added photos

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,6 +1,14 @@
 import logo from '../assets/logo.png';
+import HeaderPhoto from "../assets/Header-photo.jpg";
+import HeaderPhoto2 from "../assets/Header-photo2.jpg";
+import HeaderPhoto3 from "../assets/Header-photo3.jpg";
+import Footerbg from "../assets/Footerbg.jpg"
 
 
 export default {
-  logo
+  logo,
+  HeaderPhoto,
+  HeaderPhoto2,
+  HeaderPhoto3,
+  Footerbg
 };

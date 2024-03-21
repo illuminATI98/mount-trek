ef8945b63ce227be564707a317e26cf645f29782
Attila Dobi <attiladobi100@gmail.com>
2024-03-21 20:09:30 +0100

    Added Header to home page

--- frontend/src/container/Home/Home.jsx
+++ frontend/src/container/Home/Home.jsx
@@ -2,31 +2,13 @@ import React, { useEffect, useState } from 'react';
 import './Home.scss'; 
 import { images } from "../../constants";
 import { AnimatePresence, motion } from "framer-motion";
+import { Header } from '../../components/Header/Header';
 
 const Home = () => {
-  const [scrollY, setScrollY] = useState(0);
-
-  useEffect(() => {
-      const handleScroll = () => {
-          setScrollY(window.scrollY);
-      };
-
-      window.addEventListener('scroll', handleScroll);
-
-      return () => {
-          window.removeEventListener('scroll', handleScroll);
-      };
-  }, []);
 
   return (
       <div className="app__home">
-          <motion.div className="header-photo" style={{ y: -scrollY * 0.5 }}>
-              {}
-          </motion.div>
-          <div className="welcome-text">
-              <h1>Welcome to MountTrek</h1>
-              <p>Scroll down to explore more</p>
-          </div>
+          <Header />
       </div>
   );
 }

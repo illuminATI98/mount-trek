59a2e3dce842f8eca6e6694bd86bd0bf8a5593b4
Attila Dobi <attiladobi100@gmail.com>
2024-03-22 18:12:43 +0100

    Added dummy data for tourcards

--- frontend/src/container/Home/Home.jsx
+++ frontend/src/container/Home/Home.jsx
@@ -2,13 +2,49 @@ import React, { useEffect, useState } from 'react';
 import './Home.scss'; 
 import { images } from "../../constants";
 import { AnimatePresence, motion } from "framer-motion";
-import { Header } from '../../components/Header/Header';
+import { Header, TourCard } from '../../components';
 
 const Home = () => {
+
+    const tours = [
+        {
+            id: 1,
+            title: 'City Tour',
+            description: 'Explore the city with our guided tour.',
+            price: 50,
+            image: {}
+        },
+        {
+            id: 2,
+            title: 'Mountain Hike',
+            description: 'Hike through breathtaking mountain trails.',
+            price: 75,
+            image: {}
+        },
+        {
+            id: 3,
+            title: 'City Tour',
+            description: 'Explore the city with our guided tour.',
+            price: 50,
+            image: {}
+        },
+        {
+            id: 4,
+            title: 'Mountain Hike',
+            description: 'Hike through breathtaking mountain trails.',
+            price: 75,
+            image: {}
+        },
+    ];
 
   return (
       <div className="app__home">
           <Header />
+          <div className='app_home-card-container'>
+                {tours.map(tour => (
+                    <TourCard key={tour.id} tour={tour}/>
+                ))}
+          </div>
       </div>
   );
 }

2a0c5b720a0f7596d358a3869d461acfd2e1da41
Attila Dobi <attiladobi100@gmail.com>
2024-03-24 16:10:03 +0100

    Fixed miss aligment

--- frontend/src/components/SlideShow/SlideShow.jsx
+++ frontend/src/components/SlideShow/SlideShow.jsx
@@ -29,22 +29,24 @@ const SlideShow = () => {
 
   const dragX = useMotionValue(0);
 
-   useEffect(() => {
-     const intervalRef = setInterval(() => {
-       const x = dragX.get();
+  useEffect(() => {
+    const intervalRef = setInterval(() => {
+      const x = dragX.get();
 
-       if (x === 0) {
-         setImgIndex((pv) => {
-           if (pv === imgs.length - 1) {
-             return 0;
-           }
-           return pv + 1;
-         });
-       }
-     }, AUTO_DELAY);
+      if (x === 0) {
+        setImgIndex((pv) => {
+          if (pv === imgs.length - 1) {
+            return 0;
+          }
+          return pv + 1;
+        });
+      }
+    }, AUTO_DELAY);
 
-     return () => clearInterval(intervalRef);
-   }, []);
+    return () => clearInterval(intervalRef);
+  }, []);
+
+
 
   const onDragEnd = () => {
     const x = dragX.get();

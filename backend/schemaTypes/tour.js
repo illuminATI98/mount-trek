799fb5a66e150a2175139b423a993abf41201863
Attila Dobi <attiladobi100@gmail.com>
2024-08-17 04:12:56 +0200

    Updated tour schema

--- backend/schemaTypes/tour.js
+++ backend/schemaTypes/tour.js
@@ -3,10 +3,12 @@ export default {
   title: "Tour",
   type: "document",
   fields: [
+
+    //For tour card
     {
-      name: "image",
-      title: "Image",
-      type: "array",
+      name: "thumbnail",
+      title: "Thumbnail",
+      type: "image",
       of: [{type: "image"}],
       options: {
         hotspot: true,
@@ -18,12 +20,11 @@ export default {
       type: "string",
     },
     {
-      name: "slug",
-      title: "Slug",
-      type: "slug",
+      name: "description",
+      title: "Short Description",
+      type: "string",
       options: {
-        source: "name",
-        maxLenght: 90,
+        maxLenght: 45,
       }
     },
     {
@@ -32,14 +33,100 @@ export default {
       type: "number",
     },
     {
-      name: "details",
-      title: "Details",
-      type: "string",
-    },
-    {
       name: "date",
       title: "Date",
       type: "datetime"
-    }
+    },
+    {
+      name: "duration",
+      title: "Duration",
+      type: "number",
+    },
+    {
+      name: "availability",
+      title: "Availability",
+      type: "boolean",
+    },
+
+    //For tour page
+    
+    {
+      name: "details",
+      title: "Details",
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            {title: 'Normal', value: 'normal'},
+            {title: 'H1', value: 'h1'},
+            {title: 'H2', value: 'h2'},
+            {title: 'H3', value: 'h3'},
+            {title: 'H4', value: 'h4'},
+            {title: 'H5', value: 'h5'},
+            {title: 'H6', value: 'h6'},
+            {title: 'Quote', value: 'blockquote'}
+          ]
+        },
+        {
+          type: 'image'
+        }
+      ]
+    },
+    {
+      name: "program",
+      title: "Program",
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            {title: 'Normal', value: 'normal'},
+            {title: 'H1', value: 'h1'},
+            {title: 'H2', value: 'h2'},
+            {title: 'H3', value: 'h3'},
+            {title: 'H4', value: 'h4'},
+            {title: 'H5', value: 'h5'},
+            {title: 'H6', value: 'h6'},
+            {title: 'Quote', value: 'blockquote'}
+          ]
+        },
+        {
+          type: 'image'
+        }
+      ]
+    },
+    {
+      name: "informations",
+      title: "Additional Informations",
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            {title: 'Normal', value: 'normal'},
+            {title: 'H1', value: 'h1'},
+            {title: 'H2', value: 'h2'},
+            {title: 'H3', value: 'h3'},
+            {title: 'H4', value: 'h4'},
+            {title: 'H5', value: 'h5'},
+            {title: 'H6', value: 'h6'},
+            {title: 'Quote', value: 'blockquote'}
+          ]
+        },
+        {
+          type: 'image'
+        }
+      ]
+    },
+    {
+      name: "images",
+      title: "Images",
+      type: "array",
+      of: [{type: "image"}],
+      options: {
+        hotspot: true,
+      }
+    },
   ]
 }
